047c90bd45bc6cb66336194895bc20aa02bcf71e
Vladyslav Kyselov <vladyslav.kyselov@pitchbook.com>
2023-11-22 01:26:10 +0100

    change browser router to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './firebase/firebase.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <HashRouter>
         <App/>
-    </BrowserRouter>
+    </HashRouter>
 );
